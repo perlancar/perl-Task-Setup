15c6eabd9ce066cab9b00155a53c1686222ba12b
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-03-29 08:57:41 +0700

    Initial commit

---
+++ lib/Task/Setup.pm
@@ -0,0 +1,30 @@
+package Task::Perinci;
+
+# VERSION
+
+1;
+# ABSTRACT: Install all Setup modules
+
+=head1 DESCRIPTION
+
+This task will install all Setup modules.
+
+=pkgroup Included modules
+
+=pkg Setup
+
+=pkg Setup::File
+
+=pkg Setup::File::Symlink
+
+=pkg Setup::Multi
+
+=pkg Setup::Test
+
+=pkg Setup::Text::Snippet
+
+=pkg Setup::Unix::User
+
+=pkg Test::Setup
+
+=cut
